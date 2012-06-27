02ea55ae40f08a434bebcf76489e57816296ae9a
Victor Costa <victorcostaguitar@gmail.com>
2012-06-26 22:23:33 -0300

    Change schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120618130250) do
+ActiveRecord::Schema.define(:version => 20120627003909) do
+
+  create_table "albums", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "comments", :force => true do |t|
     t.string   "name"
